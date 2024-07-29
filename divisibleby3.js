3fd85c56412fbd7d81fceefdcfff7eec134b41aa
kbzcraft <ytkbzcraft@gmail.com>
2024-07-29 07:43:22 +0545

    added greet fun

--- divisibleby3.js
+++ divisibleby3.js
@@ -1,9 +1,12 @@
 const arr = [3, 5, 1, 2, 9, 12];
-function helloman(){
-  return "hello abhishek"
+function helloman() {
+  return "hello abhishek";
 }
-const arrr=arr.filter((item)=>{
-  return item % 3===0;
-
+const greet = (uname) => {
+  console.log(`hello welcome to broadway infosys ${uname}`);
+};
+const arrr = arr.filter((item) => {
+  return item % 3 === 0;
 });
-  console.log(arrr)
+console.log(arrr);
+
