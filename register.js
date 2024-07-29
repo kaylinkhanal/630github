39eed548d249271df91c23e361933edafcff3064
saralss <sharmasaral746@gmail.com>
2024-07-29 07:02:42 +0545

    added register page

---
+++ register.js
@@ -0,0 +1,9 @@
+const year =24
+ const day=11
+
+const output =${day}/${year}
+ console.log(output)
+const userid = '-32123-32'
+const finalOutput = userid.replaceAll('-','')
+console.log(finalOutput)
+Number(finalOutput)
